c9ccb8f6d3b1cf35e0c9bae62d6b4a39efb3fdf3
shahriarsajeeb <support@becodemy.com>
2025-09-18 12:54:08 +0600

    UI bug fixed

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import useAuthorized from '@/hooks/useAuthorized';
 import OnboardingScreen from '@/screens/onboarding';
+import { Redirect } from 'expo-router';
 import React from 'react';
 
 export default function Index() {
     const { authorized } = useAuthorized();
 
-    // if (authorized) {
-    //     return <Redirect href="/(tabs)/home" />
-    // }
+    if (authorized) {
+        return <Redirect href="/(tabs)/home" />
+    }
 
     return (
         <OnboardingScreen />
